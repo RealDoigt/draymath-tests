670374a3473dfe880255996ee3c453acb8456030
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-23 21:15:47 -0500

    test case FFFF test()

--- source/testcases.d
+++ source/testcases.d
@@ -1,4 +1,7 @@
 module text_cases;
+import std.random;
+
+enum testQty = 500;
 
 abstract class TestCase
 {
@@ -11,5 +14,21 @@ class TestCaseFFFF : TestCase
     
     this(float delegate(float, float, float) rmf, float delegate(float, float, float) dmf)
     {
+        this.rmf = rmf;
+        this.dmf = dmf;
+    }
+    
+    override bool test()
+    {
+        for (size_t i; i < testQty; ++i)
+        {
+            auto v0 = uniform!float(0f, 100f), 
+                 v1 = uniform!float(0f, 100f),
+                 v2 = uniform!float(0f, 100f);
+                 
+            if (rmf(v0, v1, v2) != dmf(v0, v1, v2)) return false;
+        }
+        
+        return true;
     }
 }
