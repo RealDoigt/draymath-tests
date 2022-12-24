f6deba0035495a5642bdbfad147a7415365f4577
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-23 21:03:57 -0500

    Update testcases.d

--- source/testcases.d
+++ source/testcases.d
@@ -1,4 +1,15 @@
+module text_cases;
+
 abstract class TestCase
 {
     bool test();
+}
+
+class TestCaseFFFF : TestCase
+{
+    private float delegate(float, float, float) rmf, dmf;
+    
+    this(float delegate(float, float, float) rmf, float delegate(float, float, float) dmf)
+    {
+    }
 }
