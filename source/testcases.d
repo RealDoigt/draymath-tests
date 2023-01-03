b428429a4c5d5d764d5e1d7aba62ad13053462ac
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-03 00:47:18 -0500

    Update testcases.d

--- source/testcases.d
+++ source/testcases.d
@@ -33,3 +33,29 @@ class TestCaseFFFF : TestCase
         return true;
     }
 }
+
+class TestCaseFFFFF : TestCase
+{
+    private float function(float, float, float, float) rmf, dmf;
+    
+    this(float function(float, float, float, float) rmf, float function(float, float, float, float) dmf)
+    {
+        this.rmf = rmf;
+        this.dmf = dmf;
+    }
+    
+    override bool test()
+    {
+        for (size_t i; i < testQty; ++i)
+        {
+            auto v0 = uniform(0f, rangeMax, r), 
+                 v1 = uniform(0f, rangeMax, r),
+                 v2 = uniform(0f, rangeMax, r),
+                 v3 = uniform(0f, rangeMax, r);
+                 
+            if (rmf(v0, v1, v2, v3) != dmf(v0, v1, v2, v3)) return false;
+        }
+        
+        return true;
+    }
+}
