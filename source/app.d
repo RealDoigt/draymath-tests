fd35b17ebf0cc87824567d999a2ea5b12572ab1b
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-03 00:35:02 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -6,10 +6,11 @@ void main()
 {
     TestCase[string] tcs;
     
-    tcs["clamp"] = 
+    tcs["Clamp"] = 
     new TestCaseFFFF(cast(float function(float, float, float))&Clamp, cast(float function(float, float, float))&dClamp);
-    
-    tcs["lerp"] = 
+    tcs["Lerp"] = 
     new TestCaseFFFF(cast(float function(float, float, float))&Lerp, cast(float function(float, float, float))&dLerp);
+    tcs["Normalize"] = 
+    new TestCaseFFFF(cast(float function(float, float, float))&Normalize, cast(float function(float, float, float))&dNormalize);
     
 } 
