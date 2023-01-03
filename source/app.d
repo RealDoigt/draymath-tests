2190b475e12eaa7bc3af58d5bad2b7083b79f70b
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-03 00:52:16 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -16,6 +16,9 @@ void main()
     new TestCaseFFFF(cast(float function(float, float, float))&Lerp, cast(float function(float, float, float))&dLerp);
     tcs["Normalize"] = 
     new TestCaseFFFF(cast(float function(float, float, float))&Normalize, cast(float function(float, float, float))&dNormalize);
+    // returns float, 4 float params
+    tcs["Remap"] = 
+    new TestCaseFFFFF(cast(float function(float, float, float, float))&Remap, cast(float function(float, float, float, float))&dRemap);
     // END Function Test Cases
     
     foreach (key, tc; tcs)
