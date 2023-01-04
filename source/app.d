d7f12f1c14119a475a5fa33bdf7044b0f89991d5
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-04 08:34:33 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -27,22 +27,22 @@ void main()
     new TestCaseV2(cast(Vector2 function())&Vector2One, cast(Vector2 function())&dVector2One);
     // returns Vector2, 2 Vector2 params
     tcs["Vector2Add"] = 
-    new TestCaseTV2V2(cast(Vector2 function(Vector2, Vector2))&Vector2Add, cast(Vector2 function(Vector2, Vector2))&dVector2Add);
+    new TestCaseTV2V2!Vector2(cast(Vector2 function(Vector2, Vector2))&Vector2Add, cast(Vector2 function(Vector2, Vector2))&dVector2Add);
     tcs["Vector2Subtract"] = 
-    new TestCaseTV2V2(cast(Vector2 function(Vector2, Vector2))&Vector2Subtract, cast(Vector2 function(Vector2, Vector2))&dVector2Subtract);
+    new TestCaseTV2V2!Vector2(cast(Vector2 function(Vector2, Vector2))&Vector2Subtract, cast(Vector2 function(Vector2, Vector2))&dVector2Subtract);
     tcs["Vector2Multiply"] = 
-    new TestCaseTV2V2(cast(Vector2 function(Vector2, Vector2))&Vector2Multiply, cast(Vector2 function(Vector2, Vector2))&dVector2Multiply);
+    new TestCaseTV2V2!Vector2(cast(Vector2 function(Vector2, Vector2))&Vector2Multiply, cast(Vector2 function(Vector2, Vector2))&dVector2Multiply);
     tcs["Vector2Divide"] = 
-    new TestCaseTV2V2(cast(Vector2 function(Vector2, Vector2))&Vector2Divide, cast(Vector2 function(Vector2, Vector2))&dVector2Divide);
+    new TestCaseTV2V2!Vector2(cast(Vector2 function(Vector2, Vector2))&Vector2Divide, cast(Vector2 function(Vector2, Vector2))&dVector2Divide);
     tcs["Vector2Reflect"] = 
-    new TestCaseTV2V2(cast(Vector2 function(Vector2, Vector2))&Vector2Reflect, cast(Vector2 function(Vector2, Vector2))&dVector2Reflect);
+    new TestCaseTV2V2!Vector2(cast(Vector2 function(Vector2, Vector2))&Vector2Reflect, cast(Vector2 function(Vector2, Vector2))&dVector2Reflect);
     // return float, 2 Vector2 params
     tcs["Vector2DotProduct"] = 
-    new TestCaseTV2V2(cast(float function(Vector2, Vector2))&Vector2DotProduct, cast(float function(Vector2, Vector2))&dVector2DotProduct);
+    new TestCaseTV2V2!float(cast(float function(Vector2, Vector2))&Vector2DotProduct, cast(float function(Vector2, Vector2))&dVector2DotProduct);
     tcs["Vector2Distance"] = 
-    new TestCaseTV2V2(cast(float function(Vector2, Vector2))&Vector2Distance, cast(float function(Vector2, Vector2))&dVector2Distance);
+    new TestCaseTV2V2!float(cast(float function(Vector2, Vector2))&Vector2Distance, cast(float function(Vector2, Vector2))&dVector2Distance);
     tcs["Vector2Angle"] = 
-    new TestCaseTV2V2(cast(float function(Vector2, Vector2))&Vector2Angle, cast(float function(Vector2, Vector2))&dVector2Angle);
+    new TestCaseTV2V2!float(cast(float function(Vector2, Vector2))&Vector2Angle, cast(float function(Vector2, Vector2))&dVector2Angle);
     // returns Vector2, Vector2 and float params
     tcs["Vector2AddValue"] = 
     new TestCaseV2V2F(cast(Vector2 function(Vector2, float))&Vector2AddValue, cast(Vector2 function(Vector2, float))&dVector2AddValue);
@@ -54,14 +54,14 @@ void main()
     new TestCaseV2V2F(cast(Vector2 function(Vector2, float))&Vector2Rotate, cast(Vector2 function(Vector2, float))&dVector2Rotate);
     // returns float, Vector2 param
     tcs["Vector2Length"] = 
-    new TestCaseTV2(cast(float function(Vector2))&Vector2Length, cast(float function(Vector2))&dVector2Length);
+    new TestCaseTV2!float(cast(float function(Vector2))&Vector2Length, cast(float function(Vector2))&dVector2Length);
     tcs["Vector2LengthSqr"] = 
-    new TestCaseTV2(cast(float function(Vector2))&Vector2LengthSqr, cast(float function(Vector2))&dVector2LengthSqr);
+    new TestCaseTV2!float(cast(float function(Vector2))&Vector2LengthSqr, cast(float function(Vector2))&dVector2LengthSqr);
     // returns Vector2, Vector2 param
     tcs["Vector2Negate"] = 
-    new TestCaseTV2(cast(Vector2 function(Vector2))&Vector2Negate, cast(Vector2 function(Vector2))&dVector2Negate);
+    new TestCaseTV2!Vector2(cast(Vector2 function(Vector2))&Vector2Negate, cast(Vector2 function(Vector2))&dVector2Negate);
     tcs["Vector2Normalize"] = 
-    new TestCaseTV2(cast(Vector2 function(Vector2))&Vector2Normalize, cast(Vector2 function(Vector2))&dVector2Normalize);
+    new TestCaseTV2!Vector2(cast(Vector2 function(Vector2))&Vector2Normalize, cast(Vector2 function(Vector2))&dVector2Normalize);
     // returns Vector2, 2 Vector2 and 1 float params
     tcs["Vector2Lerp"] = 
     new TestCaseV2V2V2F(cast(Vector2 function(Vector2, Vector2, float))&Vector2Lerp, cast(Vector2 function(Vector2, Vector2, float))&dVector2Lerp);
