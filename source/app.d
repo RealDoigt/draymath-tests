8c137a746079f0b453b36a9f936eb5440941adb4
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-09 14:34:56 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -83,6 +83,8 @@ void main()
     new TestCaseTV3V3!Vector3(cast(Vector3 function(Vector3, Vector3))&Vector3Divide, cast(Vector3 function(Vector3, Vector3))&dVector3Divide);
     tcs["Vector3Reflect"] = 
     new TestCaseTV3V3!Vector3(cast(Vector3 function(Vector3, Vector3))&Vector3Reflect, cast(Vector3 function(Vector3, Vector3))&dVector3Reflect);
+    tcs["Vector3CrossProduct"] = 
+    new TestCaseTV3V3!Vector3(cast(Vector3 function(Vector3, Vector3))&Vector3CrossProduct, cast(Vector3 function(Vector3, Vector3))&dVector3CrossProduct);
     // END Function Test Cases
     
     foreach (key, tc; tcs) "%s: %s".writefln(key, tc.test);
