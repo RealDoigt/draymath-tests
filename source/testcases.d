05cd658b06c55109b00949a5de7d137995cf9275
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-24 10:29:42 -0500

    Update testcases.d

--- source/testcases.d
+++ source/testcases.d
@@ -436,3 +436,43 @@ class TestCaseTM(T) : TestCase
         return true;
     }
 }
+
+class TestCaseMMM : TestCase
+{
+    private Matrix function(Matrix, Matrix) rmf, dmf;
+    
+    this(Matrix function(Matrix, Matrix) rmf, Matrix function(Matrix, Matrix) dmf)
+    {
+        this.rmf = rmf;
+        this.dmf = dmf;
+    }
+    
+    override bool test()
+    {
+        for (size_t i; i < testQty; ++i)
+        {
+            auto v0 = Matrix
+                      (
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), 
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), 
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r)
+                      ),
+                 v1 = Matrix
+                      (
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), 
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), 
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r), uniform(0f, rangeMax, r), uniform(0f, rangeMax, r),
+                        uniform(0f, rangeMax, r)
+                      );
+                      
+            if (rmf(v0, v1) != dmf(v0, v1)) return false;
+        }
+        
+        return true;
+    }
+}
