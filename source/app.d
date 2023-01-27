fd9f23793c8867e0e5e2002f7e56ed6325c1f64a
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-27 05:47:28 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -238,6 +238,9 @@ void main()
     // returns Vector3, Vector4 param
     tcs["QuaternionToEuler"] = 
     new TestCaseTV4!Vector3(cast(Vector3 function(Vector4))&QuaternionToEuler, cast(Vector3 function(Vector4))&dQuaternionToEuler);
+    // returns Vector4, Vector4 and Matrix params
+    tcs["QuaternionTransform"] = 
+    new TestCaseV4Transform(cast(Vector4 function(Vector4, Matrix))&QuaternionTransform, cast(Vector4 function(Vector4, Matrix))&dQuaternionTransform);
     // END Function Test Cases
     
     foreach (key; tcs.keys.sort!((a, b) => a < b)) 
